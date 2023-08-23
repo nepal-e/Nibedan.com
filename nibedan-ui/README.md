96335d0e22eb7c4256c7568bc5a507ff5f683d70
angat-yogi <angat.yogi77@gmail.com>
2023-08-23 14:03:30 -0500

    renamed nibedan correctly

--- nibedan-ui/README.md
+++ nibedan-ui/README.md
@@ -1,6 +1,6 @@
-# NibedonUi
+# NibedanUi
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.6.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.7.
 
 ## Development server
 
--- nibedan-ui/src/app/app.component.spec.ts
+++ nibedan-ui/src/app/app.component.spec.ts
@@ -2,9 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -12,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nibedon-ui'`, () => {
+  it(`should have as title 'nibedan-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nibedon-ui');
+    expect(app.title).toEqual('nibedan-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('nibedon-ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('nibedan-ui app is running!');
   });
 });
